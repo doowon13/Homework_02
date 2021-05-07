ea79b1a2a7ea9019330f4e65e620c997e7ed538a
doowon13 <endnjs2007@gmail.com>
2021-05-07 12:55:58 +0900

    관리자이름 변수에 나오게 String가공

--- app/src/main/java/com/pluto/homework_02/MainActivity.kt
+++ app/src/main/java/com/pluto/homework_02/MainActivity.kt
@@ -16,14 +16,19 @@ class MainActivity : AppCompatActivity() {
             val email = emailAdrs.text.toString()
             val password = passWrd.text.toString()
 
+            val name = "Doowon_Hur"
+
             if (email == "endnjs2007@gmail.com" && password == "endnjs2007") {
 
-                Toast.makeText(this, "관리자로 로그인되었습니다.", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "관리자 ${name}로 로그인되었습니다.", Toast.LENGTH_SHORT).show()
             }
 
             else {
                 Toast.makeText(this, "로그인에 실패하였습니다.", Toast.LENGTH_SHORT).show()
             }
+
+
+
         }
     }
 }
